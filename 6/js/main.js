3e86093cff681847dc4f51707ed9b0dd49adcf62
Keks <keks@htmlacademy.ru>
2024-07-04 10:36:01 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/main.js
@@ -0,0 +1,10 @@
+import {similarObject} from './data.js';
+import './form.js';
+import './big-picture.js';
+import './comments-loader.js';
+import './filters.js';
+
+// eslint-disable-next-line no-console
+console.log(
+  similarObject()
+);

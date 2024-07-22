1f3ef3c6871a9c6ab7cc158592384dca2983ee6d
Keks <keks@htmlacademy.ru>
2024-07-22 18:39:17 +0300

    :heavy_check_mark: Сборка #16

---
+++ 16/js/form.js
@@ -0,0 +1,31 @@
+import { body, formUpload } from './const.js';
+import {onEscKeydown } from './util.js';
+import { defaultEffect } from './slider.js';
+
+const imgUploadCancel = formUpload.querySelector('.img-upload__cancel');
+const imgUpload = formUpload.querySelector('.img-upload__input');
+const imgUploadOverlay = formUpload.querySelector('.img-upload__overlay');
+
+function openUploadOverlay () {
+  imgUploadOverlay.classList.remove('hidden');
+  body.classList.add('modal-open');
+  document.addEventListener('keydown', onEscKeydown);
+  defaultEffect();
+}
+
+function closeUploadOverlay () {
+  formUpload.reset();
+  imgUploadOverlay.classList.add('hidden');
+  body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onEscKeydown);
+}
+
+imgUpload.addEventListener('change', () => {
+  openUploadOverlay();
+});
+
+imgUploadCancel.addEventListener('click', () => {
+  closeUploadOverlay();
+});
+
+export { openUploadOverlay, closeUploadOverlay };
